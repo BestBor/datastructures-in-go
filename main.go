10a053787de8c7434b6d94a5e9a73a6e9ed15e74
BestBor <185581264+BestBor@users.noreply.github.com>
2026-01-27 18:46:33 -0500

    feat: stack package, made to operate with generics

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/BestBor/datastructures-in-go/linkedlist"
+	"github.com/BestBor/datastructures-in-go/stack"
 )
 
 func main() {
 
 	// LinkedList playtest
+	fmt.Println("LinkedList:")
 	myList := linkedlist.New[int]()
 	myList.Prepend(50)
 	myList.Prepend(40)
@@ -16,5 +18,20 @@ func main() {
 	myList.Prepend(20)
 	myList.Prepend(10)
 	fmt.Println(myList.String())
+	fmt.Println(myList)
+
+	// Stack playtest
+	fmt.Println("Stacks:")
+	myStack := stack.New[int]()
+	myStack.Push(50)
+	myStack.Push(40)
+	myStack.Push(30)
+	myStack.Push(20)
+	myStack.Push(10)
+	fmt.Println(myStack.IsEmpty())
+	fmt.Println(myStack.Len())
+	fmt.Println(myStack.Peek())
+	fmt.Println(myStack.Pop())
+	fmt.Println(myStack)
 
 }
---
+++ stack/stack.go
@@ -0,0 +1,58 @@
+package stack
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Stack[T any] struct {
+	items []T
+}
+
+func New[T any]() *Stack[T] {
+	return &Stack[T]{items: []T{}}
+}
+
+func (s *Stack[T]) Push(element T) {
+	s.items = append(s.items, element)
+}
+
+func (s *Stack[T]) Pop() T {
+
+	if len(s.items) == 0 {
+		var zero T
+		return zero
+	}
+
+	l := len(s.items) - 1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
+
+func (s *Stack[T]) Peek() T {
+	if len(s.items) == 0 {
+		var zero T
+		return zero
+	}
+	return s.items[len(s.items)-1]
+}
+
+func (s *Stack[T]) Len() int {
+	return len(s.items)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s Stack[T]) String() string {
+	var b strings.Builder
+	for i, item := range s.items {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		fmt.Fprint(&b, item)
+	}
+	return b.String()
+}
